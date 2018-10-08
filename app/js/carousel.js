e7b9a8f81db28d628aef645c47f5193c0510f1ae
Jonathan Dionne <YoungCuck@jonathans-mbp.home>
2018-10-07 18:27:50 -0700

    beta

---
+++ app/js/carousel.js
@@ -0,0 +1,33 @@
+// $(document).ready(function(){
+//   $('.carousel-skills').slick({
+//       autoplay:false,
+//       autoplaySpeed: 1500,
+//       infinite: true,
+//       pauseOnHover: false,
+//       centerMode: false,
+//       centerPadding: '0px',
+//       slidesToShow: 5.5,
+//       focusOnSelect:true,
+//       arrows:false,
+//       responsive: [
+//         {
+//           breakpoint: 768,
+//           settings: {
+//             arrows: false,
+//             centerMode: true,
+//             centerPadding: '40px',
+//             slidesToShow: 1
+//           }
+//         },
+//         {
+//           breakpoint: 480,
+//           settings: {
+//             arrows: false,
+//             centerMode: true,
+//             centerPadding: '40px',
+//             slidesToShow: 1
+//           }
+//         }
+//       ]
+//     });
+// });
--- app/js/nav.js
+++ app/js/nav.js
@@ -1,6 +1,9 @@
-var button = "#text1"
-$("#header").click(function(){
-  $(button).html('<h1 class="headtext" id="text2"><span class="letter1">J</span><span class="letter2">O</span><span class="letter3">N</span><span class="letter4">A</span><span class="letter5">T</span><span class="letter6">H</span><span class="letter7">A</span><span class="letter8">N</span></h1>');
-  $(button).css("margin-top", "20px");
-  $(".content-container").slideDown(1850);
+$(document).ready(function(){
+var button = "#text1";
+  $("#header").click(function(){
+    $(button).html('<span class="letter1">J</span><span class="letter2">O</span><span class="letter3">N</span><span class="letter4">A</span><span class="letter5">T</span><span class="letter6">H</span><span class="letter7">A</span><span class="letter8">N</span>');
+    $(button).css("margin-top", "20px");
+    $(".content-container").slideDown(1850);
+    $("html").css('min-height', '800px');
+  });
 });
